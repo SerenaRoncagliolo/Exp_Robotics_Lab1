93459ab55902fcdbdf02f51626655182d8898a7a
SerenaRoncagliolo <serena.roncagliolo@gmail.com>
2021-04-26 10:24:58 +0000

    update behavior script and Docs

--- src/behavior_manager.py
+++ src/behavior_manager.py
@@ -3,7 +3,7 @@
 ## @package behavior_manager
 # Here is implemented the state machine that controls the switch between the behaviours of the robot
 #
-# A finite-state machine is a behavior model that consists of a finite number of states. 
+# A finite-state machine (FSM) is a behavior model that consists of a finite number of states. 
 # Based on the current state and a given input the machine performs state transitions and produces outputs
 # The state machine is implemented using the smach library
 
@@ -15,6 +15,9 @@ import smach_ros
 ## class Normal_behavior
 #
 # This class implement the NORMAL behaviour of the robot pet
+# The robot moves randomly within the map 
+# - If it receives a "play" command the FSM should go into PLAY state 
+# - If the sleep timer is triggered the FSM should go into SLEEP state
 class Normal_behavior(smach.State):
 	## method init
 	#
@@ -28,6 +31,7 @@ class Normal_behavior(smach.State):
 ## class Sleep_behavior
 #
 # This class implement the SLEEP behaviour of the robot pet
+# The robot sleeps (SLEEP state)for a random period of time, then it moves to NORMAL state
 class Sleep_behavior(smach.State):
 	## method init
 	#
@@ -41,6 +45,7 @@ class Sleep_behavior(smach.State):
 ## class Play_behavior
 #
 # This class implement the PLAY behaviour of the robot pet
+# It moves the robot to the predefined (X, Y) location within the map and moves it back to the user.
 class Play_behavior(smach.State):
 	## method init
 	#
@@ -51,4 +56,29 @@ class Play_behavior(smach.State):
 	# it executes the required actions
 	def execute(self, userdata):
 
+
+def main():
+	rospy.init_node("behavior_manager")
+
+	## Create state machine	
+	sm = smach.StateMachine(outcomes=['outcome4','outcome5'])
+	## machine container
+	with sm:
+	## add states to the container,
+		smach.StateMachine.add('NORMAL', Normal_behavior(), 
+					transitions={'start_sleep':'SLEEP',
+							'start_play':'PLAY'})
+ 		smach.StateMachine.add('SLEEP', Sleep_behavior(), transitions={'stop_sleep':'NORMAL'})	
+ 		smach.StateMachine.add('PLAY', Play_behavior(), transitions={'stop_play':'NORMAL'})	
+	
+	# Execute SMACH plan
+	outcome = sm.execute()
+		
+	## Wait for ctrl-c to stop the application
+    	rospy.spin()
+    	sis.stop()
+
+
+if __name__ == "__main__":
+    main()
 
